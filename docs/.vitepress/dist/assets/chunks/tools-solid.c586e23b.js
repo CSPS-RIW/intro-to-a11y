e0e6c1078ae2fcf7eb86b012f4f5c633d314c9e2
rtorres <rene.torres@csps-efpc.gc.ca>
2022-08-03 09:24:59 -0400

    Allow dist dir in main

---
+++ docs/.vitepress/dist/assets/chunks/tools-solid.c586e23b.js
@@ -0,0 +1,1 @@
+var s="/assets/tools-solid.c5abe0fa.png";export{s as _};
---
+++ docs/.vitepress/dist/assets/resources_index.md.319db070.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o,d as t}from"./app.2e4d6f27.js";const f=JSON.parse('{"title":"Resources","description":"","frontmatter":{"sidebar":false},"headers":[],"relativePath":"resources/index.md"}'),a={name:"resources/index.md"},s=t("",2),c=[s];function n(l,i,u,d,h,p){return o(),r("div",null,c)}var m=e(a,[["render",n]]);export{f as __pageData,m as default};
